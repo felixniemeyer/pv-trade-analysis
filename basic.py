4c3f71b40281ff6c35d0a125364f6ff7e188cccc
Felix Paul Niemeyer <niemeyer.felix@gmail.com>
2021-01-29 12:18:54 +0100

    putting stuff into a function, adding colors

--- basic.py
+++ basic.py
@@ -1,4 +1,3 @@
-import matplotlib.pyplot as plt
 import sympy as sp
 
 Q_pv = sp.symbols('Q_pv', positive=True)
@@ -6,13 +5,9 @@ Q_pv = sp.symbols('Q_pv', positive=True)
 PQ_wine = sp.symbols('PQ_wine', positive=True)
 CQ_wine = sp.symbols('CQ_wine', positive=True)
 
-utility = sp.sqrt(Q_pv*CQ_wine)
+utility = Q_pv**sp.S(1/2) * CQ_wine**sp.S(1/2)
 
-production_cost = sp.sqrt((Q_pv*2)**2 + PQ_wine**2) # sp.sqrt(Q_pv**2 + PQ_wine**2)
-print(production_cost)
-
-# plot utility function
-# sp.plotting.plot3d(utility, (Q_pv, 0, 1), (CQ_wine, 0, 1))
+production_cost = sp.sqrt((Q_pv*sp.S(19/9))**sp.S(2) + PQ_wine**sp.S(2)) # sp.sqrt(Q_pv**2 + PQ_wine**2)
 
 budget = sp.symbols("budget", positive=True)
 max_utility = sp.symbols('max_utility', positive=True)
@@ -25,12 +20,12 @@ ppf = general_ppf.subs({budget: sp.S(100)})
 
 ratio = sp.diff(ppf, Q_pv)
 
-    
-(mu, qpv) = optimum = sp.solve([
+optimum = sp.solve([
     sp.Eq(ppf, general_indifference_curve), 
     sp.Eq(ratio, sp.diff(general_indifference_curve, Q_pv))
-], (max_utility, Q_pv))[0]
-
+], (max_utility, Q_pv))
+print('optimum', optimum)
+(mu, qpv) = optimum[0]
 print("maximum utility resulting from production budget:", mu)
 
 tangent = ratio.subs({Q_pv:qpv}) * ( Q_pv - qpv) + ppf.subs({Q_pv:qpv})
--- plot_autarky.py
+++ plot_autarky.py
@@ -1,47 +1,81 @@
 import sympy as sp
+import matplotlib.pyplot as plt
 
-Q_pv = sp.symbols('Q_pv', positive=True)
+# SAVE_TO_FILE = <file prefix> or False
+SAVE_TO_FILE = "plots/autarky/" 
 
-PQ_wine = sp.symbols('PQ_wine', positive=True)
-CQ_wine = sp.symbols('CQ_wine', positive=True)
+def plot_autarky(u_pv_exponent, u_wine_exponent, p_pv_coefficient, p_wine_coefficient, budget, title):
+    Q_pv = sp.symbols('Q_pv', positive=True)
+    PQ_wine = sp.symbols('PQ_wine', positive=True)
+    CQ_wine = sp.symbols('CQ_wine', positive=True)
+    max_utility = sp.symbols('max_utility', positive=True)
 
-utility = Q_pv**sp.S(1/3) * CQ_wine**sp.S(1/2)
+    utility = Q_pv**u_pv_exponent * CQ_wine**u_wine_exponent
 
-production_cost = sp.sqrt((Q_pv*sp.S(3/2))**sp.S(2) + PQ_wine**sp.S(2)) # sp.sqrt(Q_pv**2 + PQ_wine**2)
+    production_cost = sp.sqrt((Q_pv*p_pv_coefficient)**2 + (PQ_wine*p_wine_coefficient)**2) 
 
-budget = sp.symbols("budget", positive=True)
-max_utility = sp.symbols('max_utility', positive=True)
+    ppf = sp.solve(sp.Eq(production_cost, budget), PQ_wine)[0]
 
-general_ppf = sp.solve(sp.Eq(production_cost, budget), PQ_wine)[0]
+    general_indifference_curve = sp.solve(sp.Eq(utility, max_utility), CQ_wine)[0]
 
-general_indifference_curve = sp.solve(sp.Eq(utility, max_utility), CQ_wine)[0]
+    ratio = sp.diff(ppf, Q_pv)
 
-ppf = general_ppf.subs({budget: sp.S(100)})
-ratio = sp.diff(ppf, Q_pv)
+    ppf_utility = utility.subs({CQ_wine: ppf})
+    ppf_utility_d = sp.diff(ppf_utility, Q_pv)
 
-ppf_utility = utility.subs({CQ_wine: ppf})
-ppf_utility_d = sp.diff(ppf_utility, Q_pv)
+    optimum = sp.solve([
+        ppf_utility_d, 
+        sp.Eq(ppf, general_indifference_curve)
+    ],
+        (max_utility, Q_pv)
+    )
 
-optimum = sp.solve([
-    ppf_utility_d, 
-    sp.Eq(ppf, general_indifference_curve)
-],
-    (max_utility, Q_pv)
+    print(optimum)
+
+    (mu, qpv) = optimum[0]
+
+    tangent = ratio.subs({Q_pv:qpv}) * ( Q_pv - qpv) + ppf.subs({Q_pv:qpv})
+
+    plot = sp.plotting.plot(ppf, (Q_pv, 0, 150), show=False, label="ppf", line_color='#ee6321')
+    further_graphs = [
+        sp.plotting.plot(general_indifference_curve.subs({max_utility: mu}), (Q_pv, 0, 150), show=False, label="indifference curve", line_color='#3070ab'),
+        sp.plotting.plot(tangent, (Q_pv, 0, 150), show=False, label="tangent", line_color='grey'),
+    ]
+    for g in further_graphs: 
+        plot.extend(g)
+    plot.xlim = (0, 160)
+    plot.ylim = (0, 160)
+    plot.xlabel = "Q_PV"
+    plot.ylabel = "Q_wine"
+    plot.legend = True
+    # plot.title = title + "(autarky)" 
+    if(SAVE_TO_FILE != False and title != False):
+        plot.save(SAVE_TO_FILE + title + ".png")
+    plot.show()
+    
+plot_autarky(
+    sp.S(1/2),
+    sp.S(1/2),
+    sp.S(1),
+    sp.S(1),
+    sp.S(100),
+    "initial position"
 )
 
-print(optimum)
+plot_autarky(
+    sp.S(2),
+    sp.S(1/2),
+    sp.S(1),
+    sp.S(1),
+    sp.S(100),
+    "EU with PV consumption subsidies"
+)
 
-(mu, qpv) = optimum[0]
-
-tangent = ratio.subs({Q_pv:qpv}) * ( Q_pv - qpv) + ppf.subs({Q_pv:qpv})
-
-plot = sp.plotting.plot(ppf, (Q_pv, 0, 150), show=False, label="ppf")
-further_graphs = [
-    sp.plotting.plot(general_indifference_curve.subs({max_utility: mu}), (Q_pv, 0, 150), show=False, label="ic"),
-    sp.plotting.plot(tangent, (Q_pv, 0, 150), show=False, label="ic"),
-]
-for g in further_graphs: 
-    plot.extend(g)
-plot.xlim = (0, 150)
-plot.ylim = (0, 150)
-plot.show()
+plot_autarky(
+    sp.S(1/2),
+    sp.S(1/2),
+    sp.S(2/3),
+    sp.S(1),
+    sp.S(100),
+    "CN with PV production subsidies"
+)
