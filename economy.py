00a3091ed0f508bf24868a9c04202c6caed7dde4
Felix Paul Niemeyer <niemeyer.felix@gmail.com>
2021-02-01 00:22:24 +0100

    cleaning up code, now it's shiny: every code that I write is a poem

--- economy.py
+++ economy.py
@@ -2,19 +2,22 @@ import sympy as sp
 
 import colorsys
 
-def color_gen(rgb, stepfactor=1.2):
+
+def color_gen(rgb, difference=1.2):
     while True:
         yield rgb
         (h, l, s) = colorsys.rgb_to_hls(*rgb)
-        rgb = colorsys.hls_to_rgb((h+0.01) % 1, max(0, min(1, stepfactor * l)), s)
+        rgb = colorsys.hls_to_rgb(
+            (h + (difference-1.0) * 0.1) % 1, max(0, min(1, difference * l)), s)
 
-class Economy: 
+
+class Economy:
     def __init__(
         self,
-        name, 
-        product_x, product_y, 
-        utility_function, 
-        production_cost_function, 
+        name,
+        product_x, product_y,
+        utility_function,
+        production_cost_function,
         budget,
         rgb=(0, 0, 0)
     ):
@@ -24,95 +27,110 @@ class Economy:
         self.rgb = rgb
 
         # symbols
-        self.qx = sp.symbols('qx', positive=True) # quantity of good x
-        self.qx = sp.symbols('qx', positive=True) # quantity of good x
-        self.sy = sp.symbols('sy', positive=True)  # supply of good y
-        self.dy = sp.symbols('dy', positive=True)  # demand of good y 
-        self.max_utility = sp.symbols('max_utility', positive=True) # max utility that can be reached for a given budget
+        qx = sp.symbols('qx', positive=True)  # quantity of good x
+        qy = sp.symbols('qy', positive=True)  # quantity of good x
+        sq = sp.symbols('sy', positive=True)  # supply of good y
+        dq = sp.symbols('dy', positive=True)  # demand of good y
+        # max utility that can be reached for a given budget
+        max_utility = sp.symbols('max_utility', positive=True)
 
         # ppf, indifference curve, price line
-        self.utility = utility_function.subs({qy: self.dy}) 
-        self.production_cost = production_cost_function.subs({qy: self.sy})
+        self.utility = utility_function.subs({qy: dq})
+        self.production_cost = production_cost_function.subs({qy: sq})
 
-        self.ppf = sp.solve(sp.Eq(self.production_cost, budget), self.sy)[0]
+        self.ppf = sp.solve(sp.Eq(self.production_cost, budget), sq)[0]
 
         self.general_indifference_curve = sp.solve(
-            sp.Eq(self.utility, self.max_utility), self.dy)[0]
+            sp.Eq(self.utility, max_utility), dq)[0]
 
-        self.supply_price_ratio = sp.diff(self.ppf, self.qx)
+        self.supply_price_ratio = sp.diff(self.ppf, qx)
 
-        self.ppf_utility = self.utility.subs({self.dy: self.ppf})
-        self.ppf_utility_d = sp.diff(self.ppf_utility, self.qx)
+        self.ppf_utility = self.utility.subs({dq: self.ppf})
+        self.ppf_utility_d = sp.diff(self.ppf_utility, qx)
 
         self.optimum = sp.solve([
             self.ppf_utility_d,
             sp.Eq(self.ppf, self.general_indifference_curve)
         ],
-            (self.max_utility, self.qx)
+            (max_utility, qx)
         )
 
         (self.mu, self.qpv) = self.optimum[0]
 
-        self.indifference_curve = self.general_indifference_curve.subs({self.max_utility: self.mu})
+        self.indifference_curve = self.general_indifference_curve.subs(
+            {max_utility: self.mu})
 
-        self.price_line = self.supply_price_ratio.subs({self.qx: self.qpv}) * (self.qx - self.qpv) + self.ppf.subs({self.qx: self.qpv})
+        self.price_line = self.supply_price_ratio.subs(
+            {qx: self.qpv}) * (qx - self.qpv) + self.ppf.subs({qx: self.qpv})
 
         # RS and RD of good x
-        self.Q_ratio = sp.symbols('Q_ratio', positive=True) # quantity ratio Q_pv / Q_wine
-        
-        self.implied_supply_Q_pv = sp.solve([
-            sp.Eq(self.Q_ratio, self.qx / self.sy),
-            sp.Eq(self.ppf, self.sy)
-        ], (self.qx, self.sy))[0][0]
-        self.relative_supply = -self.supply_price_ratio.subs({self.qx: self.implied_supply_Q_pv})
-        
-        self.demand_price_ratio = sp.diff(self.indifference_curve, self.qx)
-        self.implied_demand_Q_pv = sp.solve([
-            sp.Eq(self.Q_ratio, self.qx / self.dy),
-            sp.Eq(self.indifference_curve, self.dy)
-        ], (self.qx, self.dy))[0][0]
-        self.relative_demand = -self.demand_price_ratio.subs({self.qx: self.implied_demand_Q_pv})
+        # the following works only if supply is convex and demand is concave
 
-    def plot_autarky(self): 
-        f = (sp.N(self.supply_price_ratio.subs({self.qx: self.qpv})) ** 2 + 1) ** -0.5
-        price_line_xrange = 50 * f
-        price_line_range = (self.qx, self.qpv - price_line_xrange, self.qpv + price_line_xrange)
+        q_ratio = sp.symbols('Q_ratio', positive=True)
+
+        self.price_implied_supply = sp.solve([
+            sp.Eq(q_ratio, qx / sq),
+            sp.Eq(self.ppf, sq)
+        ], (qx, sq))[0]
+        self.relative_supply = - \
+            self.supply_price_ratio.subs({qx: self.price_implied_supply[0]})
+
+        self.price_implied_demand = sp.diff(self.indifference_curve, qx)
+        self.implied_demand_Q_pv = sp.solve([
+            sp.Eq(q_ratio, qx / dq),
+            sp.Eq(self.indifference_curve, dq)
+        ], (qx, dq))[0]
+        self.relative_demand = - \
+            self.price_implied_demand.subs({qx: self.implied_demand_Q_pv[0]})
+
+    def plot_autarky(self, lim=(0, 1)):
+        qx = sp.symbols('qx', positive=True)  # quantity of good x
+
+        f = (sp.N(self.supply_price_ratio.subs(
+            {qx: self.qpv})) ** 2 + 1) ** -0.5
+        price_line_xrange = lim[1] / 3 * f
+        price_line_range = (qx, self.qpv - price_line_xrange,
+                            self.qpv + price_line_xrange)
 
         color = color_gen(self.rgb)
 
-        autarky_plot = sp.plotting.plot(self.ppf, (self.qx, 0, 150), show=False,
-                                label=self.country_code + " ppf", line_color=next(color))
+        autarky_plot = sp.plotting.plot(self.ppf, (qx, *lim), show=False,
+                                        label=self.name + " ppf", line_color=next(color))
         further_plots = [
-            sp.plotting.plot(self.indifference_curve, (
-                self.qx, 0, 150), show=False, label=self.country_code + " indifference curve", line_color=next(color)),
+            sp.plotting.plot(self.indifference_curve, price_line_range, show=False,
+                             label=self.name + " indifference curve", line_color=next(color)),
             sp.plotting.plot(self.price_line, price_line_range, show=False,
-                             label=self.country_code + " price line", line_color=next(color)),
+                             label=self.name + " price line", line_color=next(color)),
         ]
         for g in further_plots:
             autarky_plot.extend(g)
-        autarky_plot.xlim = (0, 160)
-        autarky_plot.ylim = (0, 160)
-        autarky_plot.xlabel = "Q_PV"
-        autarky_plot.ylabel = "Q_wine"
+
+        autarky_plot.xlim = lim
+        autarky_plot.ylim = lim
+        autarky_plot.xlabel = "Q_" + self.product_x
+        autarky_plot.ylabel = "Q_" + self.product_y
         autarky_plot.legend = True
         # plot.title = title + "(autarky)"
-                
+
         return autarky_plot
-    
-    def plot_relative(self): 
-        print(self.implied_supply_Q_pv)
-        print(self.implied_demand_Q_pv)
-        
-        plot_range = (0, 6)
-        color = color_gen(self.rgb)
-        relative_plot = sp.plotting.plot(self.relative_supply, (self.Q_ratio, *plot_range), label=self.country_code + " RS " + self.product_x, show=False, line_color=next(color))
+
+    def plot_relative(self, lim=(0, 6)):
+        q_ratio = sp.symbols('Q_ratio', positive=True)
+
+        color = color_gen(self.rgb, 1.6)
+        relative_plot = sp.plotting.plot(self.relative_supply, (q_ratio, *lim),
+                                         label=self.name + " RS " + self.product_x, show=False, line_color=next(color))
         further_plots = [
-            sp.plotting.plot(self.relative_demand, (self.Q_ratio, *plot_range), label=self.country_code + " RD " + self.product_x, show=False, line_color=next(color))
+            sp.plotting.plot(self.relative_demand, (q_ratio, *lim),
+                             label=self.name + " RD " + self.product_x, show=False, line_color=next(color))
         ]
-        for p in further_plots: 
+        for p in further_plots:
             relative_plot.extend(p)
-        relative_plot.xlim = plot_range
-        relative_plot.ylim = plot_range
+        relative_plot.xlim = lim
+        relative_plot.ylim = lim
+        relative_plot.xlabel = "Q_" + self.product_x + " / Q_" + self.product_y
+        relative_plot.ylabel = "p_" + self.product_x + " / p_" + self.product_y
+
         relative_plot.legend = True
 
         # relative_supply = sp.solve([
--- main.py
+++ main.py
@@ -5,60 +5,60 @@ from economy import Economy
 
 SAVE_PLOTS = True
 SAVE_PREFIX = "plots/"
-PLOT_AUTARKY = False
+PLOT_AUTARKY = True
 PLOT_RELATIVE = True
+DO_EXAMPLE = False
 
-(q_pv, q_wine) = sp.symbols(['qx', 'qy'])
+(q_pv, q_wine) = sp.symbols(['qx', 'qy'], positive=True)
 
-example = Economy(
-    'Honolulu', # name of the trading agent
-    'pv', 'wine', # name of goods 
-    q_pv** (1/2) * q_wine ** (1/2), # utility 
-    sp.sqrt(q_pv ** 2 + q_wine ** 2), # production_cost
-    100, # budget
+if DO_EXAMPLE:
+    example = Economy(
+        'Honolulu',  # name of the trading agent
+        'pv', 'wine',  # name of goods
+        q_pv ** (1/2) * q_wine ** (1/2),  # utility
+        sp.sqrt(q_pv ** 2 + q_wine ** 2),  # production_cost
+        100,  # budget
+    )
+    example.plot_autarky().show()
+
+eu = Economy(
+    'EU',  
+    'pv', 'wine',  
+    q_pv ** (2) * q_wine ** (1/2),  
+    sp.sqrt(q_pv ** 2 + q_wine ** 2),  
+    100,  
+    [0.1, 0.5, 0.8]
 )
 
-example.plot_autarky().show()
+cn = Economy(
+    'CN',  
+    'pv', 'wine',  
+    q_pv ** (1/2) * q_wine ** (1/2),  
+    sp.sqrt((q_pv/2) ** 2 + (q_wine) ** 2), 
+    100,  
+    [0.8, 0.0, 0.1]
+)
 
-# eu = Economy(
-#     sp.S(2),
-#     sp.S(1/2),
-#     sp.S(1),
-#     sp.S(1),
-#     sp.S(100),
-#     "EU",
-#     [0.1, 0.5, 0.8]
-# )
-# 
-# cn = Economy(
-#     sp.S(1/2),
-#     sp.S(1/2),
-#     sp.S(2/3),
-#     sp.S(3/2),
-#     sp.S(100),
-#     "CN",
-#     [0.8, 0.0, 0.1]
-# )
-# 
-# if PLOT_AUTARKY:
-#     eu_plot = eu.plot_autarky()
-#     cn_plot = cn.plot_autarky()
-#     if(SAVE_PLOTS != False):
-#         eu_plot.save(SAVE_PREFIX + "autarky/eu.png")
-#         cn_plot.save(SAVE_PREFIX + "autarky/cn.png")
-#     eu_plot.extend(cn_plot)
-#     if(SAVE_PLOTS != False):
-#         eu_plot.save(SAVE_PREFIX + "autarky/joint.png")
-#     eu_plot.show()
-#     
-# if PLOT_RELATIVE: 
-#     eu_plot = eu.plot_relative()
-#     cn_plot = cn.plot_relative()
-#     if(SAVE_PLOTS != False):
-#         eu_plot.save(SAVE_PREFIX + "relative/eu.png")
-#         cn_plot.save(SAVE_PREFIX + "relative/cn.png")
-#     eu_plot.extend(cn_plot)
-#     if(SAVE_PLOTS != False):
-#         eu_plot.save(SAVE_PREFIX + "relative/joint.png")
-#     eu_plot.show()
-# 
+if PLOT_AUTARKY:
+    lim = (0, 200)
+    eu_plot = eu.plot_autarky(lim)
+    cn_plot = cn.plot_autarky(lim)
+    if(SAVE_PLOTS != False):
+        eu_plot.save(SAVE_PREFIX + "autarky/eu.png")
+        cn_plot.save(SAVE_PREFIX + "autarky/cn.png")
+    eu_plot.extend(cn_plot)
+    if(SAVE_PLOTS != False):
+        eu_plot.save(SAVE_PREFIX + "autarky/joint.png")
+    eu_plot.show()
+
+if PLOT_RELATIVE:
+    lim = (0, 6)
+    eu_plot = eu.plot_relative(lim)
+    cn_plot = cn.plot_relative(lim)
+    if(SAVE_PLOTS != False):
+        eu_plot.save(SAVE_PREFIX + "relative/eu.png")
+        cn_plot.save(SAVE_PREFIX + "relative/cn.png")
+    eu_plot.extend(cn_plot)
+    if(SAVE_PLOTS != False):
+        eu_plot.save(SAVE_PREFIX + "relative/joint.png")
+    eu_plot.show()
